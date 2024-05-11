b186b014a6d2575b5eb811c4b69ad3ad5735f67c
Dirceu Henrique <dhnasci@gmail.com>
2024-05-10 21:58:39 -0400

    mudança para Postgre sql na produção

--- src/main/java/br/manaus/mysoft/acolherbk/AcolherbkApplication.java
+++ src/main/java/br/manaus/mysoft/acolherbk/AcolherbkApplication.java
@@ -9,14 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AcolherbkApplication implements CommandLineRunner {
 
-    private static boolean isMigration = false;
+    private static boolean isMigration = true;
     private static boolean desenvolvimento = false;
     private static final String MINHA_URL = "jdbc:sqlserver://localhost;databaseName=acolher";
-    private static final String MINHA_URL_PROD = "jdbc:sqlserver://eu-az-sql-serv1.database.windows.net:1433;database=d3yn0bi94s8zvey";
+    private static final String MINHA_URL_PROD = "jdbc:postgresql://cf9gid2f6uallg.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/dbq6n1jnmrlssa";
     private static final String MEU_LOGIN = "sa";
-    private static final String MEU_LOGIN_PROD = "umg0nch6d8p4brs";
+    private static final String MEU_LOGIN_PROD = "u5fmceib9av9a3";
     private static final String SENHA = "MeuChapa15";
-    private static final String SENHA_PROD = "J9rr7TkGpaTfrL4XjTZhqsMbf";
+    private static final String SENHA_PROD = "p9bdd34295d1f2256dbf82df7ac3c37332965262edbf6855bd0da1fce35ecaa99";
     private static final String FLYWAY_LOCATION = "classpath:db/migration" ;
 
 
