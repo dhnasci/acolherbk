a0365c841661fbe4cb165f2682ac3d682a6aae2a
Dirceu Henrique <dhnasci@gmail.com>
2025-07-25 09:41:13 -0400

    ajustes para docker compose

--- src/main/java/br/manaus/mysoft/acolherbk/AcolherbkApplication.java
+++ src/main/java/br/manaus/mysoft/acolherbk/AcolherbkApplication.java
@@ -9,14 +9,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AcolherbkApplication implements CommandLineRunner {
 
-    private static boolean isMigration = true;
-    private static boolean desenvolvimento = true;
-    private static final String MINHA_URL = "jdbc:postgresql://localhost:5432/postgres";
-    private static final String MINHA_URL_PROD = "jdbc:postgresql://c97r84s7psuajm.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d4jgr2qoeh16lc";
-    private static final String MEU_LOGIN = "postgres";
-    private static final String MEU_LOGIN_PROD = "u5fmceib9av9a3";
-    private static final String SENHA = "Selenio17!";
-    private static final String SENHA_PROD = "p7ea5c6562070b4d4f6739169290d9f9465486085c76d1ca967d78762a4542f73";
+    private static boolean isMigration = false;
+    private static final String MINHA_URL = System.getenv("SPRING_DATASOURCE_URL");
+    private static final String MEU_LOGIN = System.getenv("SPRING_DATASOURCE_USERNAME");
+    private static final String SENHA = System.getenv("SPRING_DATASOURCE_PASSWORD");
     private static final String FLYWAY_LOCATION = "classpath:db/migration" ;
 
 
@@ -24,17 +20,10 @@ public class AcolherbkApplication implements CommandLineRunner {
         if (isMigration) {
             Flyway flyway = null;
             try {
-                if (desenvolvimento) {
                     flyway = Flyway.configure()
                             .dataSource(MINHA_URL, MEU_LOGIN, SENHA)
                             .locations(FLYWAY_LOCATION)
                             .load();
-                } else {
-                    flyway = Flyway.configure()
-                            .dataSource(MINHA_URL_PROD, MEU_LOGIN_PROD, SENHA_PROD)
-                            .locations(FLYWAY_LOCATION)
-                            .load();
-                }
 
             } catch (Exception e) {
                 throw new ObjetoException("Problema no flyway");
